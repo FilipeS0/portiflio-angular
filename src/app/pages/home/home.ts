c52feadf2f85c8fdf55dc9f9f523967e5c78a956
Filipe Souto <filipesoutocosta@gmail.com>
2026-01-18 17:31:12 -0300

    when I do have more projects I'll add here

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MenuHeader } from '../../components/menu-header/menu-header';
 import { Presentation } from '../../components/presentation/presentation';
 import { About } from '../../components/about/about';
+import { Projects } from '../../components/projects/projects';
 
 @Component({
   selector: 'app-home',
-  imports: [MenuHeader, Presentation, About],
+  imports: [MenuHeader, Presentation, About, Projects],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
